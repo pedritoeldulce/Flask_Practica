e4c1c4ede1334c996230d0c9c222cf68508df2e6
pedritoeldulce <franceskolyperez@gmail.com>
2021-03-18 21:50:29 -0500

    Hola Mundo

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,6 +1,11 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return "<h1>Hola mundo desde el servidor de Flask</h1>"
+
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug= True, port=9000)

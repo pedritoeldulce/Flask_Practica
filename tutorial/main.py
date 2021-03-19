75795f09ba8dd9fef0cc2b43285c83f855c88bb6
pedritoeldulce <franceskolyperez@gmail.com>
2021-03-18 22:04:26 -0500

    Paginas dinamicas

--- tutorial/main.py
+++ tutorial/main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    name = "paolo"
+    course = "Python Web"
+    return render_template("index.html", username=name, course_name=course)
+
 
 if __name__ == "__main__":
     app.run(debug= True, port=9000)
